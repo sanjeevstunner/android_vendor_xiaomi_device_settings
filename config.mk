82e1174c0eb0ea1c823e5bdc3343d645c95267a4
kubersharma001 <kubersharma001@gmail.com>
2020-08-11 21:52:13 +0530

    vayu: Make sure Doze shows up.
    Change-Id: I9eac20699b21b7205fa852ec9a72c137d49ab4f1

--- config.mk
+++ config.mk
@@ -2,5 +2,8 @@ PRODUCT_PACKAGES += DeviceParts
 
 VENDOR_PATH := vendor/xiaomi/devicesettings
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(VENDOR_PATH)/overlay
+
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
