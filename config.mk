05d610e9c49ffb5d86398564a2fbc0308261bb65
vansh123456 <vanshbhatnagar13@gmail.com>
2021-01-31 13:52:48 +0530

    vayu: build vendor.xiaomi.hardware.touchfeature@1.0

--- config.mk
+++ config.mk
@@ -4,3 +4,8 @@ VENDOR_PATH := vendor/xiaomi/devicesettings
 
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+
+# Touchfeature
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.touchfeature@1.0 \
+    vendor.xiaomi.hardware.touchfeature@1.0.vendor
