4dfb4e82d49552b8d47f35fe896d44c717cd8509
sanjeevstunner <sanjeevstunner1@gmail.com>
2022-04-17 07:19:25 +0530

    Revert "[SQUASH] Remove haptic feedback level adjustment"
    This reverts commit 501ab97840acc3598e3b874170ae75b9d33b5367

--- src/org/lineageos/settings/BootCompletedReceiver.java
+++ src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,6 +26,7 @@ import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.haptic.HapticUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -45,5 +46,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
+        HapticUtils.restoreLevel(context);
     }
 }
