72fcdd1df32c28408aa2ac68737ebbd469e6d92e
Sanjeev <sanjeevstunner1@gmail.com>
2022-07-03 17:29:39 +0530

    Drop Xiaomi TouchFeature HAL definitions
    *will be added from hardware repo
    
    Revert "vayu: Import Xiaomi TouchFeature HAL definitions"
    This reverts commit afcbdefa418cd23349f281708f8f6b72367825e9.
    
    Revert "vayu: build vendor.xiaomi.hardware.touchfeature@1.0"
    This reverts commit 05d610e9c49ffb5d86398564a2fbc0308261bb65.
    
    Revert "vayu: parts: interfaces: Adapt Touch Feature HALs for current path"
    This reverts commit f42c479dfc5b430bd47b3dbf7e06a82a7974fb6f.

--- config.mk
+++ config.mk
@@ -4,8 +4,3 @@ VENDOR_PATH := vendor/xiaomi/devicesettings
 
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
-
-# Touchfeature
-PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.touchfeature@1.0 \
-    vendor.xiaomi.hardware.touchfeature@1.0.vendor
